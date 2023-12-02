bf4ba60e3e81c608c6ed1cef2146f9c0d2cfb0a3
Nguyễn Đức Anh <datnguyen1854@gmail.com>
2023-12-03 05:07:37 +0700

    ducanh

--- app/src/main/java/com/example/maphistory/MapFragment.java
+++ app/src/main/java/com/example/maphistory/MapFragment.java
@@ -1,8 +1,16 @@
 package com.example.maphistory;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +21,10 @@ import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link MapFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class MapFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,8 +35,10 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public HomeFragment() {
+
+    public MapFragment() {
         // Required empty public constructor
+
     }
 
     /**
@@ -39,6 +49,7 @@ public class HomeFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment HomeFragment.
      */
+    private MyMapFragment myMapFragment;
 
 
     ImageView img;
@@ -49,8 +60,8 @@ public class HomeFragment extends Fragment {
 
 
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static MapFragment newInstance(String param1, String param2) {
+        MapFragment fragment = new MapFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,13 +77,16 @@ public class HomeFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
+        FragmentManager fragmentManager = this.getActivity().getSupportFragmentManager();
+        this.myMapFragment = (MyMapFragment) fragmentManager.findFragmentById(R.id.fragment_map);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
         return view;
 
 
