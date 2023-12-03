510c07c4e26401e99cf7f0f435ed607d04ed6b92
Nguyễn Đức Anh <datnguyen1854@gmail.com>
2023-12-03 20:14:12 +0700

    moi

--- app/src/main/java/com/example/maphistory/InforFragment.java
+++ app/src/main/java/com/example/maphistory/InforFragment.java
@@ -1,5 +1,6 @@
 package com.example.maphistory;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import com.example.maphistory.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +31,14 @@ public class InforFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+
+    Activity context;
+    ActivityMainBinding binding;
+    ListAdapter listAdapter;
+    ArrayList<ListData> dataArrayList = new ArrayList<>();
+    ListData listData;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -53,12 +73,14 @@ public class InforFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        context = getActivity();
         return inflater.inflate(R.layout.fragment_infor, container, false);
     }
 }
