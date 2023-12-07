797b0aa5276cbecb805fae0ca1941f624ac6223e
atsayu <quyenhoangntt03@gmail.com>
2023-12-07 19:38:08 +0700

    Fix login and register

--- app/src/main/java/com/example/maphistory/LoginActivity.java
+++ app/src/main/java/com/example/maphistory/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.maphistory;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
     TextView signUpText, forget;
     EditText loginEmail, loginPassword;
 
+    FirebaseAuth fAuth;
+
     DatabaseHelper databaseHelper;
 
 
@@ -28,39 +36,73 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
-        databaseHelper = new DatabaseHelper(this);
-
+//        databaseHelper = new DatabaseHelper(this);
+        fAuth = FirebaseAuth.getInstance();
         btn1_login = findViewById(R.id.loginButton);
         signUpText = findViewById(R.id.signupText);
         forget = findViewById(R.id.forgetText);
         loginEmail = findViewById(R.id.username);
         loginPassword = findViewById(R.id.password);
+
         btn1_login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //Khai báo intent
-//                Intent intentLogin = new Intent(LoginActivity.this,MainActivity.class);
-//                //khoi dong
-//                startActivity(intentLogin);
-//            }
             @Override
-            public void onClick(View view) {
-                String email = loginEmail.getText().toString();
-                String password = loginPassword.getText().toString();
-                if(email.equals("")||password.equals(""))
-                    Toast.makeText(LoginActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
-                else{
-                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
-                    if(checkCredentials == true){
-                        Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
-                        Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                    }else{
-                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
-                    }
+            public void onClick(View v) {
+                String emailText = loginEmail.getText().toString();
+                String passText = loginPassword.getText().toString();
+
+                if (TextUtils.isEmpty(emailText)) {
+                    loginEmail.setError("Vui lòng nhập email");
+                }
+                else if (TextUtils.isEmpty(passText)) {
+                    loginEmail.setError("Vui lòng nhập mật khẩu");
+                }
+
+                else if (passText.length() < 6) {
+                    loginPassword.setError("Mật khẩu cần có độ dài ít nhất 6 ký tự");
+                }
+
+                else {
+                    fAuth.signInWithEmailAndPassword(emailText, passText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Lỗi không thể đăng nhập:" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
                 }
             }
         });
+
+//        btn1_login.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                //Khai báo intent
+////                Intent intentLogin = new Intent(LoginActivity.this,MainActivity.class);
+////                //khoi dong
+////                startActivity(intentLogin);
+////            }
+//            @Override
+//            public void onClick(View view) {
+//                String email = loginEmail.getText().toString();
+//                String password = loginPassword.getText().toString();
+//                if(email.equals("")||password.equals(""))
+//                    Toast.makeText(LoginActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+//                else{
+//                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
+//                    if(checkCredentials == true){
+//                        Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+//                        Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
+//                        startActivity(intent);
+//                    }else{
+//                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            }
+//        });
         signUpText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/maphistory/SignupActivity.java
+++ app/src/main/java/com/example/maphistory/SignupActivity.java
@@ -1,62 +1,159 @@
 package com.example.maphistory;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.maphistory.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SignupActivity extends AppCompatActivity {
 
-    ActivitySignupBinding binding;
-    DatabaseHelper databaseHelper;
+//    ActivitySignupBinding binding;
+//    DatabaseHelper databaseHelper;
+
+    EditText email;
+    EditText pass;
+    EditText confirmPass;
+    EditText name;
+    EditText phone;
+    FirebaseAuth fAuth;
+    Button signUpButton;
+    String userID;
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySignupBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        databaseHelper = new DatabaseHelper(this);
-        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_signup);
+        email = findViewById(R.id.signup_email);
+        pass = findViewById(R.id.signup_password);
+        confirmPass = findViewById(R.id.signup_confirm);
+        signUpButton = findViewById(R.id.signup_button);
+        name = findViewById(R.id.signup_name);
+        phone = findViewById(R.id.signup_phone);
+
+        fAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String email = binding.signupEmail.getText().toString();
-                String password = binding.signupPassword.getText().toString();
-                String confirmPassword = binding.signupConfirm.getText().toString();
-                if(email.equals("")||password.equals("")||confirmPassword.equals(""))
-                    Toast.makeText(SignupActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
-                else{
-                    if(password.equals(confirmPassword)){
-                        Boolean checkUserEmail = databaseHelper.checkEmail(email);
-                        if(checkUserEmail == false){
-                            Boolean insert = databaseHelper.insertData(email, password);
-                            if(insert == true){
-                                Toast.makeText(SignupActivity.this, "Signup Successfully!", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                                startActivity(intent);
-                            }else{
-                                Toast.makeText(SignupActivity.this, "Signup Failed!", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                String emailText = email.getText().toString();
+                String passText = pass.getText().toString();
+                String confirmPassText = confirmPass.getText().toString();
+
+                if (TextUtils.isEmpty(emailText)) {
+                    email.setError("Vui lòng nhập email");
+                }
+                else if (TextUtils.isEmpty(passText)) {
+                    pass.setError("Vui lòng nhập mật khẩu");
+                }
+                else if (TextUtils.isEmpty(confirmPassText)) {
+                    confirmPass.setError("Vui lòng nhập xác nhận mật khẩu");
+                }
+
+                else if (passText.length() < 6) {
+                    pass.setError("Mật khẩu cần có độ dài ít nhất 6 ký tự");
+                }
+
+                else if (!passText.equals(confirmPassText)) {
+                     confirmPass.setError("Mật khẩu không trùng khớp");
+                }
+                else {
+                    fAuth.createUserWithEmailAndPassword(emailText, passText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignupActivity.this, "Tạo thành công tài khoản", Toast.LENGTH_SHORT).show();
+                                userID = fAuth.getCurrentUser().getUid();
+                                String fullName = name.getText().toString();
+                                String phoneNumber = phone.getText().toString();
+                                DocumentReference documentReference = firestore.collection("users").document(userID);
+                                Map<String, String> user = new HashMap<>();
+                                user.put("fname", fullName);
+                                user.put("phone", phoneNumber);
+                                user.put("email", emailText);
+                                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Log.d("TAG", "user created: " + userID );
+                                    }
+                                });
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            } else {
+                                Toast.makeText(SignupActivity.this, "Lỗi thể tạo tài khoản: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                             }
                         }
-                        else{
-                            Toast.makeText(SignupActivity.this, "User already exists! Please login", Toast.LENGTH_SHORT).show();
-                        }
-                    }else{
-                        Toast.makeText(SignupActivity.this, "Invalid Password!", Toast.LENGTH_SHORT).show();
-                    }
+                    });
                 }
+
+
             }
         });
-        binding.loginRedirectText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+
+//        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        databaseHelper = new DatabaseHelper(this);
+//        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String email = binding.signupEmail.getText().toString();
+//                String password = binding.signupPassword.getText().toString();
+//                String confirmPassword = binding.signupConfirm.getText().toString();
+//                if(email.equals("")||password.equals("")||confirmPassword.equals(""))
+//                    Toast.makeText(SignupActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+//                else{
+//                    if(password.equals(confirmPassword)){
+//                        Boolean checkUserEmail = databaseHelper.checkEmail(email);
+//                        if(checkUserEmail == false){
+//                            Boolean insert = databaseHelper.insertData(email, password);
+//                            if(insert == true){
+//                                Toast.makeText(SignupActivity.this, "Signup Successfully!", Toast.LENGTH_SHORT).show();
+//                                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+//                                startActivity(intent);
+//                            }else{
+//                                Toast.makeText(SignupActivity.this, "Signup Failed!", Toast.LENGTH_SHORT).show();
+//                            }
+//                        }
+//                        else{
+//                            Toast.makeText(SignupActivity.this, "User already exists! Please login", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }else{
+//                        Toast.makeText(SignupActivity.this, "Invalid Password!", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            }
+//        });
+//        binding.loginRedirectText.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
